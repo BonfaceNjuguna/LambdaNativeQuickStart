8cba4ba22497b8346e45f16bc21d701da654e607
Bonface Njuguna <bonfacenjuguna13@gmail.com>
2025-08-17 09:44:56 +0300

    Refactor build workflow to install OpenJDK 17 and streamline Android SDK setup; update UI definitions for expense tracker

--- ExpenseTracker/main.scm
+++ ExpenseTracker/main.scm
@@ -1,92 +1,87 @@
-;; SPDX identifier: GPL-3.0-or-later
+(define dbfile (string-append (system-directory) "/expense.sqlite"))
+(define db #f)
 
-;; DB file
-(define dbfile "expense.sqlite")
+;; --- UI Definition ---
+(define demouiform:expense-tracker
+  (list->table
+    `(
+      (main
+        "Expense Tracker"
+        #f  ;; No menu
+        #f  ;; No icon
+        (hbox
+          (label text "Welcome" align left fnt 'bigfnt)
+          (spacer)
+          (button text "Logout" action ,(lambda () (terminate))))
+        (spacer height 10)
+        (label text "Total Balance" align left fnt 'fnt)
+        (hbox
+          (label id totalbalance text ,(lambda ()
+                                         (let* ((income (or (caar (sqlite-query db "SELECT SUM(amount) FROM transactions WHERE type='Income'")) 0))
+                                                (expense (or (caar (sqlite-query db "SELECT SUM(amount) FROM transactions WHERE type='Expense'")) 0))
+                                                (total (- income expense)))
+                                           (number->string total)))
+                 align left fnt 'hdfnt)
+          (label text " KES" align left fnt 'smlfnt))
+        (spacer height 20)
+        (hbox
+          (vbox
+            (hbox
+              (label text "Income" align left fnt 'fnt)
+              (label text "↑" color Green align left fnt 'fnt))
+            (label id incomeamount text ,(lambda ()
+                                           (number->string (or (caar (sqlite-query db "SELECT SUM(amount) FROM transactions WHERE type='Income'")) 0)))
+                   align left fnt 'hdfnt))
+          (spacer width 40)
+          (vbox
+            (hbox
+              (label text "Expenses" align left fnt 'fnt)
+              (label text "↓" color Red align left fnt 'fnt))
+            (label id expenseamount text ,(lambda ()
+                                            (number->string (or (caar (sqlite-query db "SELECT SUM(amount) FROM transactions WHERE type='Expense'")) 0)))
+                   align left fnt 'hdfnt)))
+        (spacer height 20)
+        (label text "Transaction History" align left fnt 'fnt)
+        (listbox id transactionhistory entries ,(lambda ()
+                                                  (let ((rows (sqlite-query db "SELECT type, amount, description FROM transactions ORDER BY rowid DESC")))
+                                                    (map (lambda (row)
+                                                           (string-append (car row) ": "
+                                                                          (number->string (cadr row)) " KES - "
+                                                                          (caddr row)))
+                                                         rows))))
+        (spacer height 20)
+        (button text "Add" fnt 'bigfnt action ,(lambda () 'addtransaction))
+      )
 
-;; GUI handles
+      (addtransaction
+        "Add Transaction"
+        ("Back" main)
+        #f
+        (spacer height 10)
+        (dropdown id type text "Type:" entries ("Income" "Expense") #t location ui default 0)
+        (textentry id amount text "Amount:" keypad numint #t location ui default "0")
+        (textentry id description text "Description:" #t location ui default "")
+        (button text "Save"
+          action ,(lambda ()
+                    (let* ((t (uiget 'type "Income"))
+                           (amt (string->number (uiget 'amount "0")))
+                           (desc (uiget 'description "")))
+
+                      (if (and amt (> amt 0))
+                          (begin
+                            ;; Save to DB
+                            (sqlite-query db (string-append
+                              "INSERT INTO transactions (type, amount, description) VALUES ('"
+                              t "'," (number->string amt) ",'" desc "')"))
+                            ;; No reload needed, lambdas will query fresh on main
+                            'main)
+                          #f))))))))
+
+;; --- GUI Setup ---
 (define gui #f)
 (define form #f)
 
-;; Function: reload data from DB into UI widgets
-(define (reload-from-db)
-  (let* ((db (sqlite-open dbfile))
-         (rows (sqlite-query db
-                "SELECT type, amount, description FROM transactions ORDER BY rowid DESC"))
-         (income (sqlite-query db
-                  "SELECT SUM(amount) FROM transactions WHERE type='Income'"))
-         (expense (sqlite-query db
-                   "SELECT SUM(amount) FROM transactions WHERE type='Expense'"))
-         (incomeamt (if (and income (car income) (car (car income)))
-                        (car (car income)) 0))
-         (expenseamt (if (and expense (car expense) (car (car expense)))
-                         (car (car expense)) 0))
-         (total (- incomeamt expenseamt)))
-    (sqlite-close db)
-    (glgui-widget-set! gui form 'transactionhistory
-      (map (lambda (row)
-             (string-append (car row) ": "
-                            (number->string (cadr row)) " KES - "
-                            (caddr row)))
-           rows))
-    (glgui-widget-set! gui form 'incomeamount (number->string incomeamt))
-    (glgui-widget-set! gui form 'expenseamount (number->string expenseamt))
-    (glgui-widget-set! gui form 'totalbalance (number->string total))))
-
-;; UI definition using list->table
-(define expense-tracker
-  (list->table
-   `(
-     (main
-       "Expense Tracker"
-       #f
-       #f
-       (label text "Total Balance")
-       (label id totalbalance text "0")
-       (label text "KES")
-       (spacer height 20)
-       (label text "Income")
-       (label id incomeamount text "0")
-       (spacer height 10)
-       (label text "Expenses")
-       (label id expenseamount text "0")
-       (spacer height 20)
-       (label text "Transaction History")
-       (listbox id transactionhistory entries ())
-       (spacer height 20)
-       (button text "Add" action ,(lambda () 'addtransaction))
-     )
-
-     (addtransaction
-       "Add Transaction"
-       ("Back" main)
-       #f
-       (dropdown id type label "Type:" entries ("Income" "Expense"))
-       (textentry id amount text "Amount:" keypad numint)
-       (textentry id description text "Description:")
-       (button text "Save"
-         action ,(lambda ()
-                   (let* ((t (uiget 'type "Income"))
-                          (amt (string->number (uiget 'amount "0")))
-                          (desc (uiget 'description "")))
-                     (if (and amt (> amt 0))
-                         (begin
-                           (let ((db (sqlite-open dbfile)))
-                             (sqlite-query db
-                               (string-append
-                                "INSERT INTO transactions (type, amount, description) VALUES ('"
-                                t "'," (number->string amt) ",'" desc "')"))
-                             (sqlite-close db))
-                           (reload-from-db)
-                           (glgui-widget-set! gui form 'amount "")
-                           (glgui-widget-set! gui form 'description "")
-                           'main)
-                         #f))))
-     )
-   )))
-
-;; Main entry point
 (main
-  ;; Initialization
   (lambda (w h)
     (make-window 480 800)
     (glgui-orientation-set! GUI_PORTRAIT)
@@ -94,36 +89,27 @@
 
     (let ((aw (glgui-width-get))
           (ah (glgui-height-get)))
-      (glgui-box gui 0 0 aw ah Blue)
+      (glgui-box gui 0 0 aw ah DarkGreen)
       (set! form (glgui-uiform gui 0 0 aw ah)))
 
-    ;; Ensure the DB and table exist
-    (let ((db (sqlite-open dbfile)))
-      (sqlite-query db
-        "CREATE TABLE IF NOT EXISTS transactions (type TEXT, amount INTEGER, description TEXT)")
-      (sqlite-close db))
+    (set! db (sqlite-open dbfile))
+    (sqlite-query db
+      "CREATE TABLE IF NOT EXISTS transactions (type TEXT, amount INTEGER, description TEXT)")
 
     (glgui-widget-set! gui form 'sandbox (system-directory))
-    (glgui-widget-set! gui form 'uiform expense-tracker)
+    (glgui-widget-set! gui form 'uiform demouiform:expense-tracker)
 
-    ;; Use a default font
-    (glgui-widget-set! gui form 'fnt ascii_14.fnt)
+    ;; Fonts
+    (glgui-widget-set! gui form 'fnt ascii_18.fnt)
+    (glgui-widget-set! gui form 'smlfnt ascii_14.fnt)
+    (glgui-widget-set! gui form 'hdfnt ascii_24.fnt)
+    (glgui-widget-set! gui form 'bigfnt ascii_40.fnt)
 
-    ;; Load initial data
-    (reload-from-db))
-
-  ;; Event handler
+  )
   (lambda (t x y)
-    (if (and (= t EVENT_KEYPRESS) (= x EVENT_KEYESCAPE))
-        (terminate))
+    (if (= t EVENT_KEYPRESS)
+        (if (= x EVENT_KEYESCAPE) (terminate)))
     (glgui-event gui t x y))
-
-  ;; Termination handler
-  (lambda ()
-    #t)
-
-  ;; Suspend
+  (lambda () (when db (sqlite-close db)) #t)
   (lambda () (glgui-suspend))
-
-  ;; Resume
   (lambda () (glgui-resume)))
